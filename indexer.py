f9241ae4856788d7f6a39d6caab617c1455bbb49
Ian Blaise <iblaise@uci.edu>
2020-02-22 14:58:57 -0800

    Some necessary functions added

--- indexer.py
+++ indexer.py
@@ -1,4 +1,32 @@
 import posting as posting
 import bs4 as bs
+import os #allows us to get the directories and file names
 
-#:)
+# returns all of the sites in a directory
+def getSites(directory):
+	print(directory)
+	return list()
+
+# returns all of the directories in a directory
+def getDirectories(directory):
+	return os.listdir(directory)
+
+#returns the HTML content of a page (JSON parse completed)
+def getPageContent(site):
+	print(site)
+	return ''
+
+#returns the DEV folder, used a function so that it works on windows, mac, linux..
+def startingDirectory():
+	# Formats the path correctly for Windows or some unix-like system
+	if os.name == 'nt':
+		return os.getcwd() + '\\DEV\\'
+	else:
+		return os.getcwd() + '/DEV/';
+
+def run():
+	directories = getDirectories(startingDirectory())
+	for dir in directories:
+		print(dir)	
+
+run()	
