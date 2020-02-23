93e02430596b39e1a4c61401796bcdd9582b75df
Ian Blaise <iblaise@uci.edu>
2020-02-22 17:33:14 -0800

    Reads JSON

--- indexer.py
+++ indexer.py
@@ -1,13 +1,28 @@
 import posting as posting
 import bs4 as bs
 import os  # allows us to get the directories and file names
+import json
 
-def test():
+def extractHtmlFromJson(filePath):
+    json_data = open(filePath)
+    encoding_types = {}
+    #print('Loading data from: ' + filePath)
+    try:
+        data = json.load(json_data)
+        #print(data['url'])
+        #print(data['encoding'])
+        print(data['content']) # <-- Tokenize this
+         
+    except ValueError as e:
+        return
+
+# runs through all directories and prints out a list of files within them.
+def traverseDirectories():
     for (root, dirs, files) in os.walk('./DEV', topdown=True):
         for file in files:
-            print('Tokenize and index me: ' + str(root) + str(file))
+            extractHtmlFromJson(root + '/' + file)
 
 def run():
-        test()
-
+        traverseDirectories()
+            
 run()
