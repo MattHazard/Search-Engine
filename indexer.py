6171962b257f7ca2b6102ffb98a071389ba2d7d7
Ian Blaise <iblaise@uci.edu>
2020-02-22 18:27:26 -0800

    Added nltk, testing tokenizer

--- indexer.py
+++ indexer.py
@@ -3,25 +3,27 @@ import bs4 as bs
 from bs4 import BeautifulSoup
 import os  # allows us to get the directories and file names
 import json
+import nltk
 
-
-
+#if this line is causing errors but you have nltk installed
+#open idle and do 'import nltk' then 'nltk.download('punkt')
+from nltk.tokenize import word_tokenize
 def extractHtmlFromJson(filePath):
     json_data = open(filePath)
-    encoding_types = {}
     #print('Loading data from: ' + filePath)
     try:
+        # { url, content, encoding }
         data = json.load(json_data)
-        #print(data['url'])
-        #print(data['encoding'])
-        #print(data['content']) # <-- Tokenize this
+        #load the html into BeautifulSoup
         soup = BeautifulSoup(data['content'])
+
+        #strip out the javscript and some style tags from the html file
         for garbage in soup(['script', 'style']):
             garbage.decompose()
-        
-        text = soup.get_text()
-        
-        print(text)
+        text = soup.get_text() 
+        listOfTokens = word_tokenize(text)
+
+        print(listOfTokens)
     except ValueError as e:
         return
 
@@ -32,6 +34,6 @@ def traverseDirectories():
             extractHtmlFromJson(root + '/' + file)
 
 def run():
-        traverseDirectories()
-            
+    traverseDirectories()
+
 run()
