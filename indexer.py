33ac8f241aeeef94f372893831ab6c03eed87d64
Ian Blaise <iblaise@uci.edu>
2020-02-22 16:48:28 -0800

    Much simpler..

--- indexer.py
+++ indexer.py
@@ -2,37 +2,12 @@ import posting as posting
 import bs4 as bs
 import os  # allows us to get the directories and file names
 
-
-# returns all of the sites in a directory
-def getSites(directory):
-    print(directory)
-    return list()
-
-
-# returns all of the directories in a directory
-def getDirectories(directory):
-    return os.listdir(directory)
-
-
-# returns the HTML content of a page (JSON parse completed)
-def getPageContent(site):
-    print(site)
-    return ''
-
-
-# returns the DEV folder, used a function so that it works on windows, mac, linux..
-def startingDirectory():
-    # Formats the path correctly for Windows or some unix-like system
-    if os.name == 'nt':
-        return os.getcwd() + '\\DEV\\'
-    else:
-        return os.getcwd() + '/DEV/'
-
+def test():
+    for (root, dirs, files) in os.walk('./DEV', topdown=True):
+        for file in files:
+            print('Tokenize and index me: ' + str(root) + str(file))
 
 def run():
-    directories = getDirectories(startingDirectory())
-    for dir in directories:
-        print(dir)
-
+        test()
 
 run()
