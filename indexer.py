4d7c9558c23f5426384ff74fc699035dfdd73e70
tren97 <lexjreyty@gmail.com>
2020-03-02 16:59:17 -0800

    Added code to create folder if it doesnt exist

--- indexer.py
+++ indexer.py
@@ -24,6 +24,11 @@ else:
     os.mkdir(os.getcwd() + '/' + 'DocIdMap')
     currentIndexFile = open('./DocIdMap/' + str(currentFileNum) + '.txt', 'a')
 
+if os.path.isdir('./indexes'):
+    pass
+else:
+    os.mkdir(os.getcwd() + '/' + 'indexes')
+
 ###################################################
 # loads all pickle dicts in a file to be loaded to
 # a single dictionary.
@@ -212,13 +217,13 @@ def traverseDirectories():
 def run():
     # print("Testing Tf-Idf for doc that has tf of 0.03 (3/100) and appears 1000 times out of 10,000,000 size corpus" + str(getTfIdf(.03, 10000000, 1000)))
     # traverseDirectories()
-    # extractTokensFromJson('DEV/scale_ics_uci_edu/d93a8cb31884b6fcb38d121d07176dc6752e5bf1889b3b8fa313672028a65824.json')
-    # extractTokensFromJson('DEV/dynamo_ics_uci_edu/0c961803ef7f746bd7a4f5faf3e134546dec9a75719c214bfea2ee2652e5f241.json')
-    # extractTokensFromJson('DEV/cml_ics_uci_edu/0f32f6f497d71106ff8e3a26fdf59a538771b01bed110afc8cbdc23ba804818a.json')
+    extractTokensFromJson('DEV/scale_ics_uci_edu/d93a8cb31884b6fcb38d121d07176dc6752e5bf1889b3b8fa313672028a65824.json')
+    extractTokensFromJson('DEV/dynamo_ics_uci_edu/0c961803ef7f746bd7a4f5faf3e134546dec9a75719c214bfea2ee2652e5f241.json')
+    extractTokensFromJson('DEV/cml_ics_uci_edu/0f32f6f497d71106ff8e3a26fdf59a538771b01bed110afc8cbdc23ba804818a.json')
 
     ###Loads file after it has been generated.
-    loadall('indexes/s.pickle')
-    print(words)
+    # loadall('indexes/s.pickle')
+    # print(words)
 
 
 if __name__ == "__main__":
