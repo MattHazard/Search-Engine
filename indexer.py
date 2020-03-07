b42ed5514f77bf57558c459d5ba4fc39e6297319
Tyler Reynolds <lexjreyty@gmail.com>
2020-03-06 18:32:36 -0800

    Try this shit again

--- indexer.py
+++ indexer.py
@@ -277,8 +277,8 @@ def run():
     # extractTokensFromJson('DEV/scale_ics_uci_edu/d93a8cb31884b6fcb38d121d07176dc6752e5bf1889b3b8fa313672028a65824.json')
     # extractTokensFromJson('DEV/dynamo_ics_uci_edu/0c961803ef7f746bd7a4f5faf3e134546dec9a75719c214bfea2ee2652e5f241.json')
     # extractTokensFromJson('DEV/cml_ics_uci_edu/0f32f6f497d71106ff8e3a26fdf59a538771b01bed110afc8cbdc23ba804818a.json')
-    # with open('./indexes/' + str(currPickleFile) + '.pickle', 'wb') as handle:
-    #     pickle.dump(words, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    with open('./indexes/' + str(currPickleFile) + '.pickle', 'wb') as handle:
+        pickle.dump(words, handle, protocol=pickle.HIGHEST_PROTOCOL)
     # with open('pickle.pickle', 'ab') as handle:
     #     pickle.dump(words, handle, protocol=pickle.HIGHEST_PROTOCOL)
     ###Loads file after it has been generated.
