e34bd84fd5a4bb5ddef81331eba97c758c5f3050
Matthew Mueller <mrm.mue@gmail.com>
2020-03-13 22:09:03 -0700

    fully indexed

--- search.py
+++ search.py
@@ -31,11 +31,20 @@ def search(query):
     # do and operation on all the postings by doc id to find docs with all the tokens
     # Those urls are contenders
     # Don't need tf-idf working just yet.
+
+    if query == '':
+        print('Query was empty')
+        return
     tic = time.perf_counter()
     query = que.split(' ')
     d = {}
+    currChar = ''
     for i in query:
-        temp = loadall('finalIndexes/' + str(i[0]).lower() + '.pickle')
+        if currChar != str(i[0]).lower():
+            temp = loadall('finalIndexes/' + str(i[0]).lower() + '.pickle')
+        currChar = str(i[0]).lower()
+        if stemmer.stem(i.lower()) not in temp.keys():
+            continue
         for val in temp[stemmer.stem(i.lower())]['postings']:
             if val in d:
                 d[val] += temp[stemmer.stem(i.lower())]['postings'][val]['tfidf']
@@ -44,37 +53,13 @@ def search(query):
         sort = sorted(d.items(), key=lambda x: x[1], reverse=True)
         for i, val in enumerate(sort):
             if i == 10:
+                toc = time.perf_counter()
+                print('Query performed in ' + str(toc - tic) + 'seconds')
                 return
             else:
                 print(getUrlFromDocId(val[0]))
-
-
-
-    # li = []
-    # for i in query:
-    #     d = {}
-    #     if str(i[0]).isalpha():
-    #         words = loadall('finalIndexes/' + str(i[0]).lower() + '.pickle')
-    #     else:
-    #         words = loadall('finalIndexes/numsyms.pickle')
-    #     # print(words[stemmer.stem(i.lower())]['postings'])
-    #     for j in words[stemmer.stem(i.lower())]['postings']:
-    #         d[j] = 1
-    #     li.append(d)
-    # new = {}
-    # for i in li:
-    #     print(i)
-    #     for j in i:
-    #         if j in new.keys():
-    #             new[j] += 1
-    #         else:
-    #             new[j] = 1
-    # urlL = []
-    # for i in new:
-    #     if new[i] is len(li):
-    #         urlL.append(getUrlFromDocId(int(i)))
-    # print(urlL)
     toc = time.perf_counter()
+    print('No results found')
     print('Query performed in ' + str(toc - tic) + 'seconds')
 
 
